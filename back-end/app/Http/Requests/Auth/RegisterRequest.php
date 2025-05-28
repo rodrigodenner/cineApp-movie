8a7334ac087bc76d99bcd67324aaa9cabf66bc45
Rodrigo Denner <rodrigodenner.art@gmail.com>
2025-05-28 16:10:49 -0300

    fix: refactor RegisterRequest

--- back-end/app/Http/Requests/Auth/RegisterRequest.php
+++ back-end/app/Http/Requests/Auth/RegisterRequest.php
@@ -19,4 +19,26 @@ class RegisterRequest extends FormRequest
             'password' => 'required|min:6|confirmed',
         ];
     }
+
+    public function attributes()
+    {
+        return [
+            'name'     => 'nome',
+            'email'    => 'e-mail',
+            'password' => 'senha',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required'     => 'O campo :attribute é obrigatório.',
+            'email.required'    => 'O campo :attribute é obrigatório.',
+            'email.email'       => 'O campo :attribute deve ser um endereço de e-mail válido.',
+            'email.unique'      => 'O :attribute já está em uso.',
+            'password.required' => 'O campo :attribute é obrigatório.',
+            'password.min'      => 'A :attribute deve ter pelo menos :min caracteres.',
+            'password.confirmed'=> 'A confirmação da :attribute não corresponde.',
+        ];
+    }
 }
