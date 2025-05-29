dc727e42a83362934d8caa9901147567b160cd79
Rodrigo Denner <rodrigodenner.art@gmail.com>
2025-05-29 02:02:31 -0300

    fix: map genres correctly in movie detail response

--- back-end/app/Http/Resources/TMDBMovieResource.php
+++ back-end/app/Http/Resources/TMDBMovieResource.php
@@ -13,11 +13,25 @@ class TMDBMovieResource extends JsonResource
         return [
             'id'           => $this['id'],
             'title'        => $this['title'],
-            'poster_path'  => $this['poster_path'] ? rtrim(config('movie-tmdb.tmdb.image_base_url'), '/') . $this['poster_path'] : null,
-            'genres'       => GenreMapperService::map($this['genre_ids'] ?? []),
+            'poster_path'  => $this['poster_path']
+                ? rtrim(config('movie-tmdb.tmdb.image_base_url'), '/') . $this['poster_path']
+                : null,
+            'genres'       => $this->mapGenres(),
             'overview'     => $this['overview'],
             'release_date' => $this['release_date'],
             'vote_average' => $this['vote_average'],
         ];
+    }
+
+    protected function mapGenres(): array
+    {
+        if (isset($this['genres']) && is_array($this['genres'])) {
+            return collect($this['genres'])
+                ->pluck('name')
+                ->filter()
+                ->values()
+                ->toArray();
+        }
+        return GenreMapperService::map($this['genre_ids'] ?? []);
     }
 }
